88346d50b931f75ae56539c9e494b2cf1fae3309
Dmitry Bubnov <dbubnov@e1z3r4p19.42.us.org>
2019-06-11 12:42:22 -0700

    move_next

--- fillit/move_next.c
+++ fillit/move_next.c
@@ -6,7 +6,7 @@
 /*   By: dbubnov <dbubnov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 14:40:33 by dbubnov           #+#    #+#             */
-/*   Updated: 2019/06/10 17:56:27 by dbubnov          ###   ########.fr       */
+/*   Updated: 2019/06/11 10:40:14 by dbubnov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,47 @@ static void	move_next(char f[4][5], int size_field)
 {
 	unsigned int i;
 	unsigned int j;
-	unsigned int move;
-	unsigned int flag;
 
-	move = 0;
 	i = size_field;
 	j = size_field;
 	while (i > 0 || j > 0)
 	{
-		move = 0;
-		if (f[j][i] == '#')
-		{
-			ft_swap(&f[j][i], &f[j][move]);
-			move++;
-		}
-		if (move && i != 0)
-			move++;
+		if (f[j][size_field] == '#')
+			break;
+		if (f[j][i] == '#' && i != size_field)
+			ft_swap(&f[j][i], &f[j][i + 1]);
 		if (i == 0)
 		{
 			j--;
 			i = size_field;
 		}
-		i--;
+		else
+			i--;
 	}
+	if (f[j][size_field] != '#')
+		ft_swap(&f[j][i], &f[j][i + 1]);
+}
+
+static void	move_down(char f[4][5], int size_field)
+{
+	unsigned int i;
+	unsigned int j;
+
+	i = size_field;
+	j = size_field;
+	while (i > 0 || j > 0 )
+	{
+		if (f[j][i] == '#' && i != size_field)
+			ft_swap(&f[j][i], &f[j + 1][i]);
+		if (i == 0)
+		{
+			j--;
+			i = size_field;
+		}
+		else
+			i--;
+	}
+	ft_swap(&f[j][i], &f[j + 1][i]);
 }
 
 int main(void)// compile with swap.c
@@ -51,7 +69,10 @@ int main(void)// compile with swap.c
                          "...."};
     // move_up(arr);
     // move_left(arr);
-	move_next(arr, 4);
+
+
+	// move_down(arr, 3);
+	move_next(arr, 3);
     while (i < 4)
     	printf("%s\n", arr[i++]);
 	return (0);
