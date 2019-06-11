56191f5a1d42b23ad1deb4166b824cb62324688d
Dmitry Bubnov <dbubnov@e1z3r4p19.42.us.org>
2019-06-11 12:55:53 -0700

    git_pop

--- fillit/is_overlap.c
+++ fillit/is_overlap.c
@@ -3,10 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   is_overlap.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vinograd <vinograd@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dbubnov <dbubnov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 12:45:31 by vinograd          #+#    #+#             */
+<<<<<<< Updated upstream
 /*   Updated: 2019/06/11 12:53:04 by vinograd         ###   ########.fr       */
+=======
+/*   Updated: 2019/06/11 12:54:14 by dbubnov          ###   ########.fr       */
+>>>>>>> Stashed changes
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +21,16 @@ int		is_overlap(char **board, char **figure)
 	if (board == NULL)
 		return (-1);
 	return (0);
+}
+
+
+
+int function(a, b);
+{
+	a + b;
+}
+
+int function(a, b);
+{
+	m + k;
 }
