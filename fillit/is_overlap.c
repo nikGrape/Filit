ebde10419e51d44ca47ad4b82bd4240178927f35
Nikolai Vinogradov <vinograd@e1z3r5p1.42.us.org>
2019-06-11 12:53:30 -0700

    overlap

--- fillit/is_overlap.c
+++ fillit/is_overlap.c
@@ -6,11 +6,15 @@
 /*   By: vinograd <vinograd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 12:45:31 by vinograd          #+#    #+#             */
-/*   Updated: 2019/06/11 12:46:28 by vinograd         ###   ########.fr       */
+/*   Updated: 2019/06/11 12:53:04 by vinograd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fillit.h"
+
 int		is_overlap(char **board, char **figure)
 {
+	if (board == NULL)
+		return (-1);
 	return (0);
 }
